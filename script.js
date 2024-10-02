0c595b6135e6b8f0c0e142e1f0a959fd87411d4e
Abel <abelmathew1504@gmail.com>
2024-10-02 22:36:29 +0530

    Completed UI and scripting

--- script.js
+++ script.js
@@ -1,69 +1,87 @@
-function getPlayerChoice() {
-    let choice = prompt("Choose rock, paper or scissors:","rock");
-    return choice.toLowerCase();
-}
-
 function getComputerChoice() {
-    let choice = Math.floor(Math.random()*3);
+    const choice = Math.floor(Math.random()*3);
     switch (choice) {
         case 0:
             return 'rock';
         case 1:
             return 'paper';
         case 2:
-            return 'scissors';    
+            return 'scissors';
         default:
             return 'error';
     }
 }
 
-function playRound() {
+function playRound(choice) {
     const computerSelection = getComputerChoice();
-    const humanSelection = getPlayerChoice();
-    console.log(humanSelection);
     console.log(computerSelection);
-    switch (humanSelection) {
-        case 'rock':
-            if(computerSelection === 'paper')
-                return -1;
-            else if(computerSelection === 'scissors')
-                return 1;
-            break;
-        case 'paper':
-            if(computerSelection === 'rock')
-                return 1;
-            else if(computerSelection === 'scissors')
-                return -1;
-            break;
-        case 'scissors':
-            if(computerSelection === 'paper')
-                return 1;
-            else if(computerSelection === 'rock')
-                return -1;
-            break;
-        default:
-            break;
+    hand.src = `./${computerSelection}.png`;
+    const playerSelection = choice;
+    if(playerSelection === computerSelection) {
+        outcome.textContent = "Draw!";
     }
-    return 0;
+    else {
+        switch (playerSelection) {
+            case 'rock':
+                if(computerSelection === 'paper') {
+                    computerScore++;
+                    outcome.textContent = "You lose!";
+                }
+                else if(computerSelection === 'scissors') {
+                    playerScore++;
+                    outcome.textContent = "You win!";
+                }
+                break;
+            case 'paper':
+                if(computerSelection === 'rock') {
+                    playerScore++;
+                    outcome.textContent = "You win!";
+                }
+                else if(computerSelection === 'scissors') {
+                    computerScore++;
+                    outcome.textContent = "You lose!";
+                }
+                break;
+            case 'scissors':
+                if(computerSelection === 'paper') {
+                    playerScore++;
+                    outcome.textContent = "You win!";
+                }
+                else if(computerSelection === 'rock') {
+                    computerScore++;
+                    outcome.textContent = "You lose!";
+                }
+                break;
+        }
+    }
+    score.textContent = `${playerScore}-${computerScore}`;
 }
 
-function playGame(num) {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i=0;i<num;i++) {
-        if(playRound() === 1) {
-            console.log("You win this round!");
-            playerScore++;
-        }
-        else if(playRound() === -1) {
-            console.log("You lose this round!");
-            computerScore++;
-        }
-        else {
-            console.log("You draw this round!");
-        }
-        console.log(`Score: ${playerScore}-${computerScore}`);
-    }
+const button = document.querySelectorAll("button");
+const score = document.querySelector("#score");
+
+const outcome = document.querySelector("#outcome");
+const hand = document.querySelector("#robot");
+
+for (b of button) {
+    b.addEventListener("click",(e) => playRound(e.target.classList[0]));
 }
 
-playGame(5);
+let playerScore = 0;
+let computerScore = 0;
+
+// for (let i=0;i<num;i++) {
+//     if(playRound() === 1) {
+//         console.log("You win this round!");
+//         playerScore++;
+//     }
+//     else if(playRound() === -1) {
+//         console.log("You lose this round!");
+//         computerScore++;
+//     }
+//     else {
+//         console.log("You draw this round!");
+//     }
+//     console.log(`Score: ${playerScore}-${computerScore}`);
+// }
+
